62b4af4d1c2f324e2e6abf82f2be1b465354bcf3
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:49:12 -0700

    Add files via upload
    22 Question

---
+++ Q22.js
@@ -0,0 +1,6 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
+//to produce an index error. Make sure you correct the error before closing the program.
+const days = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[6]);
+console.log(days[7]);
+export {};
