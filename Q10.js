6036e693893f162a105204a6d65855cdc27ef2ea
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:33:28 -0700

    Add files via upload
    This is my 10th Question

---
+++ Q10.js
@@ -0,0 +1,16 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment
+// to each. If you don’t have anything specific to write
+//because your programs are too simple at this point, just add your name and the current
+// date at the top of each program file. Then write one sentence
+//describing what the program does.
+let a = 20;
+let b = 10;
+//Addition:
+console.log(`${a} + ${b} = ${a + b}`);
+//Subtraction:
+console.log(`${a} - ${b} = ${a - b}`);
+//Multiplication:
+console.log(`${a} * ${b} = ${a * b}`);
+//Division:
+console.log(`${a} / ${b} = ${a / b}`);
+export {};
