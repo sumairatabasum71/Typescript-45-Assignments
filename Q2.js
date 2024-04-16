4f42fa40eec8b31c930678bdb6cacc1c6d4c84e9
sumairatabasum71 <alialiya678@gmail.com>
2024-04-16 08:14:29 -0700

    first commit

---
+++ Q2.js
@@ -0,0 +1,6 @@
+"use strict";
+//Personal Message: Store a person’s name in a variable, and print a message to that person.
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+Object.defineProperty(exports, "__esModule", { value: true });
+let firstName = "Eric";
+console.log(`Hello ${firstName} “would you like to learn some Python today?”`);
