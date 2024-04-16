f790a1912b15abe897e35abe6aac9373a74805d2
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:26:13 -0700

    Add files via upload
    This is my 5th Question

---
+++ Q5.js
@@ -0,0 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name
+// in a variable called famous_person. Then compose your message and store it in a new
+// variable called message. Print your message.
+var famous_person = "Walt Disney";
+var message = " The way to get started is to quit talking and begin doing.";
+console.log("".concat(famous_person, " once Said, \u201C").concat(message, "\u201D"));
---
+++ Q5.ts
@@ -0,0 +1,7 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name
+// in a variable called famous_person. Then compose your message and store it in a new
+// variable called message. Print your message.
+
+let famous_person:string="Walt Disney";
+let message:string="The way to get started is to quit talking and begin doing.";
+console.log(`${famous_person} once Said, “${message}”`);
