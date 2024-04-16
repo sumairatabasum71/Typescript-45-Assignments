ddd1f12ee3b410bbf4d45c6fd36184bd8aa340c9
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:24:05 -0700

    Add files via upload
    This is my 3rd Question

---
+++ Q3.ts
@@ -0,0 +1,15 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name
+// in lowercase, uppercase, and titlecase.
+
+//Uppercase:
+let firstName:string="suMAiRa";
+console.log(firstName.toUpperCase());
+
+//Lowercase:
+console.log(firstName.toLowerCase());
+
+//titlecase:
+console.log(firstName.charAt(3));
+
+
+
