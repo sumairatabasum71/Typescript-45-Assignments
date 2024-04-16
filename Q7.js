4c0a01ef859f43404326ea89e4223f17788da7d5
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:31:28 -0700

    Add files via upload
    This is my 7th Question

---
+++ Q7.js
@@ -0,0 +1,15 @@
+"use strict";
+//Number Eight: Write addition, subtraction, multiplication, and division operations that
+// each result in the number 8. Be sure to enclose your operations in print statements
+// to see the results.
+Object.defineProperty(exports, "__esModule", { value: true });
+//Addition:
+let a = 9;
+let b = 5;
+console.log(a + b);
+//Subtraction:
+console.log(a - b);
+//Multiplication:
+console.log(a * b);
+//Division:
+console.log(a / b);
---
+++ Q7.ts
@@ -0,0 +1,18 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that
+// each result in the number 8. Be sure to enclose your operations in print statements
+// to see the results.
+
+//Addition:
+let a:number= 9;
+let b:number=5;
+console.log(a+b);
+
+//Subtraction:
+console.log(a-b);
+
+//Multiplication:
+console.log(a*b);
+
+//Division:
+console.log(a/b);
+
