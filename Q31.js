d08070091c6925442f480ebf84be452843c65d2f
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:55:33 -0700

    Add files via upload
    31 Question

---
+++ Q31.js
@@ -0,0 +1,22 @@
+//No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+//• If the list is empty, print the message We need to find some users!
+//• Remove all of the usernames from your array, and make sure the correct message is printed.
+// let user_names:string[]=["Muawiya","Ahmed","Ali","Hasnain"];
+// if(user_names.length === 0) {
+//     console.log("We need to find some users!");
+// }
+// else{
+//     for(let i=0; i < user_names.length; i++);
+// }
+// console.log(user_names);
+//2nd method:
+let user_names = [];
+if (user_names.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    for (let i = 0; i < user_names.length; i++)
+        ;
+}
+console.log(user_names);
+export {};
