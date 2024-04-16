589908367edf5f0d36cbf4216e119d79a5a37ed9
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:41:41 -0700

    Add files via upload
    14th Question

---
+++ Q14.js
@@ -0,0 +1,7 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+// Make a list that includes at least three people you’d like to
+//invite to dinner. Then use your list to print a message to each person, inviting them 
+//to dinner.
+let guestList = ["Atif", "Bilal", "Sabiha", "Agha", "Asad", "Tehmina"];
+guestList.map((items) => (console.log(`Dear ${items}, You are invited to a Dinner`)));
+export {};
