3cf201259a47dd1306c8e8770d3c58e6860d48e4
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:25:18 -0700

    Add files via upload
    This is my 4th Question

---
+++ Q4.js
@@ -0,0 +1,9 @@
+"use strict";
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the
+// name of its author. Your output should look something like the following,
+//including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+Object.defineProperty(exports, "__esModule", { value: true });
+let famousPerson = "Steve Jobs Says";
+console.log(`${famousPerson} “Your time is limited, so don't waste it living someone else's life. 
+Don't be trapped by dogma which is living with the results of other people's thinking.”`);
---
+++ Q4.ts
@@ -0,0 +1,8 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the
+// name of its author. Your output should look something like the following,
+//including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let famousPerson: string = "Steve Jobs Says";
+console.log(`${famousPerson} “Your time is limited, so don't waste it living someone else's life. 
+Don't be trapped by dogma which is living with the results of other people's thinking.”`);
