42f92dbb7c3383460e495c495a7596463972383a
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:35:54 -0700

    Add files via upload
    This is my 13th Question

---
+++ Q13.js
@@ -0,0 +1,6 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle
+// or a car, and make a list that stores several examples. Use your list
+//to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let transportation = ["Honda motorcycle", "BMW Cars", "Audi", "Rivo", "Crown Bike"];
+transportation.map((items) => console.log(`I would like to own a ${items}`));
+export {};
