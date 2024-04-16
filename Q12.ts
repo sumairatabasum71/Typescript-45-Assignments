2df61b085b31c3aedf1cbb69ef7a63f0b7ad4536
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:35:05 -0700

    Add files via upload
    This is my 12th Question

---
+++ Q12.ts
@@ -0,0 +1,11 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing 
+//each person’s name, print a message to them. The text of each message
+//should be the same, but each message should be personalized with the person’s name.
+
+let names:string[]=["Ali","Shumaila","Uzair","Sundas","Saad"];
+let message:string="Every challenge is an opportunity to become stronger.";
+console.log(names[0] +" "+ message);
+console.log(names[1] +" "+ message);
+console.log(names[2] +" "+ message);
+console.log(names[3] +" "+ message);
+console.log(names[5] +" "+ message);
