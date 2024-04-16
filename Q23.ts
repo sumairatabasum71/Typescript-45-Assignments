ff5a0bb287fe79575ed934660aca502c9c1d5e43
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:49:48 -0700

    Add files via upload
    23 Question

---
+++ Q23.ts
@@ -0,0 +1,65 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement
+//describing each test and your prediction for the results of each test. Your code
+//should look something like this:
+
+//test 1:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru');
+
+//test 2:
+console.log("Is car == 'Toyota'? I predict False.")
+console.log(car == 'Toyota');
+
+//test 3:
+console.log("is car === 'subaru'? I pridict True");
+console.log(car === 'subaru');
+
+//test 4:
+console.log("is car === 'subaru'? I pridict false");
+console.log(car === 'subaru');
+
+//test 5:
+console.log("is car.lenght > 0? i pridict true");
+console.log(car.length > 0);
+
+//test 6:
+console.log("is car.lenght < 5? i pridict false");
+console.log(car.length < 5);
+
+//test 7:
+console.log("is car.toupperCase()=== 'subaru'?i pridict true");
+console.log(car.toUpperCase()=== "SUBARU");
+
+//test 8:
+console.log("is car.toupperCase()=== 'subaru'?i pridict true");
+console.log(car.toUpperCase()=== 'subaru');
+
+//TEST 9
+console.log("is car.tolowerCase()=== 'subaru'?i pridict true");
+console.log(car.toLowerCase()=== 'subaru');
+
+//test 10:
+console.log("is car.includes('u')? i pridict true");
+console.log(car.includes('u'));
+
+
+
+
+
+
+
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+
+
+
+
+
+
+
+
+
+
