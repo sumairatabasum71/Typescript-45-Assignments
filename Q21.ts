a6c73e627d4662f57afbc62964b28212ed4bc694
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:48:37 -0700

    Add files via upload
    21 Question

---
+++ Q21.ts
@@ -0,0 +1,17 @@
+//They think of something you could store in a TypeScript Object. Write a program
+// that creates Objects containing these items.
+
+let person={
+    name:"Sumaira",
+    fname:"Sarfraz",
+    age:28,
+    cource:"Typescript"
+}
+console.log(person);
+
+let product={
+    name:"Luxury Ring,Necklace,Earing",
+    price:1899,
+    Review:4.1,
+}
+console.log(product);
