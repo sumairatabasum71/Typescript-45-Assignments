79eef5c3a090ff46b33922078d7eab98fff21805
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 09:03:00 -0700

    Add files via upload
    43 Question

---
+++ Q43.js
@@ -0,0 +1,14 @@
+//Unchanged Magicians: Start with your work from Exercise 40. Call the
+//function make_great() with a copy of the array of magicians’ names. Because the
+//original array will be unchanged, return the new array and store it in a separate array.
+//Call show_magicians() with each array to show that you have one array of the original
+//names and one array with the Great added to each magician’s name.
+function show_magicians(Magicians) {
+    for (const magician of Magicians) {
+        console.log(magician);
+        console.log(magician + " " + "The Great");
+    }
+}
+const magicianNames = ["ayaz", "fayaz", "madad", "aliya"];
+show_magicians(magicianNames);
+export {};
