35d497225e32ef1ef70e6fd36a447900d4b1f2f1
sumairatabasum71 <160247457+sumairatabasum71@users.noreply.github.com>
2024-04-16 08:32:52 -0700

    Add files via upload
    This is my 9th Question

---
+++ Q9.js
@@ -0,0 +1,6 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let favorite_Number = 2022;
+console.log(`My favourite number is ${favorite_Number}`);
---
+++ Q9.ts
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+
+let favorite_Number:number=2022;
+console.log(`My favourite number is ${favorite_Number}`);
